dad1e0ba4b13d87e43053c10d912cb3f1b5f0030
Victor Ribeiro <vvictorrib@gmail.com>
2020-08-12 12:07:37 -0300

    Added colors helper

---
+++ src/components/helper/Colors.js
@@ -0,0 +1,8 @@
+export const A = "#E0B165"; //HEADER COLOR
+export const B = "#EBA150";
+export const C = "#D48853";
+export const D = "#EB8050"; //FOOTER COLOR
+export const E = "#E06A4C";
+
+export const blue = "#3091db";
+export const white = "#fffbed";
