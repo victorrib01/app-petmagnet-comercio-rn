aa5576c4660f4216c4a0ca37693b236e96a39199
Victor Ribeiro <vvictorrib@gmail.com>
2020-08-12 19:30:31 -0300

    Colors.js fixed

--- src/components/helper/Colors.js
+++ src/components/helper/Colors.js
@@ -1,7 +1,7 @@
 export const headerColor = "#E0B165"; //HEADER COLOR
-export const B = "#EBA150";
 export const footerColor = "#D48853";
-export const D = "#EB8050"; //FOOTER COLOR
-export const inactiveColor = "#E06A4C";
 export const blue = "#3091db";
 export const white = "#fffbed";
+export const A = "#EBA150";
+export const B = "#EB8050"; //FOOTER COLOR
+export const C = "#E06A4C";
