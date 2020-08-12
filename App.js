7159c01ae22f3fe51c60033af33c03a0eb7dfe71
Victor Ribeiro <vvictorrib@gmail.com>
2020-08-12 02:46:07 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Main from './src/Main';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
+
+export default App;
