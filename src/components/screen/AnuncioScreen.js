42bdb3bbead8f7f1ccac32b257b86cadf4971b0c
Victor Ribeiro <vvictorrib@gmail.com>
2020-08-14 20:35:15 -0300

    Publicacoes and anuncio updated

--- src/components/screen/AnuncioScreen.js
+++ src/components/screen/AnuncioScreen.js
@@ -13,24 +13,26 @@ const AnuncioScreen = () => {
             O que você gostaria de anunciar?
           </Text>
           <Text style={styles.textDesc}>
-            Digite um título para chamar a atenção dos clientes, exemplo
+            Digite um título para chamar a atenção dos clientes, exemplo:
             "Aproveite esses Ótimos Preços"
           </Text>
         </View>
         <View style={styles.inputBox}>
           <Text style={styles.titulo}>Título</Text>
           <TextInput
-            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, width:300}}
+            maxLength= {45}
+            defaultValue='Um petisco para seu Dog!'
           />
           <Text style={styles.paginate}>1/4</Text>
         </View>
         <View style={styles.confirmButton}>
-          <Button title="Confirmar" onPress />
+          <Button title="Confirmar" />
         </View>
       </View>
     </View>
   );
-}
+};
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
@@ -58,7 +60,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     alignContent: 'center',
     padding: 20,
-    marginTop: 50,
+    marginTop: 20,
     marginBottom: 70,
   },
   titulo: {
@@ -72,7 +74,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   confirmButton: {
-    borderRadius: 50
-  }
+    borderRadius: 50,
+  },
 });
 export default AnuncioScreen;
--- src/components/screen/PublicacoesScreen.js
+++ src/components/screen/PublicacoesScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, ScrollView, StyleSheet, Image} from 'react-native';
 
 import Header from './../home/HomeHeader';
 
@@ -7,25 +7,39 @@ const PublicacoesScreen = ({navigation}) => {
   return (
     <View>
       <Header />
-      <View style={styles.publicacaoView}>
+      <ScrollView>
         <View style={styles.publicacaoBox}>
           <View style={styles.visivelBox}>
-            <Text style={styles.visivelFrom}>Visível a Partir de: </Text>
-            <Text style={styles.statsFrom}>15.06.2020</Text>
-            <Text style={styles.visivelUntil}>Disponível até: </Text>
-            <Text style={styles.statsUntil}>20.06.2020</Text>
+            <View style={styles.visivelFromBox}>
+              <Text style={styles.visivelFrom}>Visível a Partir de: </Text>
+              <Text style={styles.statsFrom}>15.06.2020</Text>
+            </View>
+            <View style={styles.visiveUntillBox}>
+              <Text style={styles.visivelUntil}>Disponível até: </Text>
+              <Text style={styles.statsUntil}>20.06.2020</Text>
+            </View>
           </View>
+          <Text style={styles.textBox}>Grande Promoção de Ração para seu Cachorro</Text>
           <View style={styles.fotoBox}>
             <View style={styles.item}>
-              <Text>Foto</Text>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
               <Text>R$ 38,49</Text>
             </View>
             <View style={styles.item}>
-              <Text>Foto</Text>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
               <Text>R$ 42,50</Text>
             </View>
             <View style={styles.item}>
-              <Text>Foto</Text>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
               <Text>R$ 40,99</Text>
             </View>
           </View>
@@ -37,22 +51,36 @@ const PublicacoesScreen = ({navigation}) => {
         </View>
         <View style={styles.publicacaoBox}>
           <View style={styles.visivelBox}>
-            <Text style={styles.visivelFrom}>Visível a Partir de: </Text>
-            <Text style={styles.statsFrom}>15.06.2020</Text>
-            <Text style={styles.visivelUntil}>Disponível até: </Text>
-            <Text style={styles.statsUntil}>20.06.2020</Text>
+            <View style={styles.visivelFromBox}>
+              <Text style={styles.visivelFrom}>Visível a Partir de: </Text>
+              <Text style={styles.statsFrom}>15.06.2020</Text>
+            </View>
+            <View style={styles.visiveUntillBox}>
+              <Text style={styles.visivelUntil}>Disponível até: </Text>
+              <Text style={styles.statsUntil}>20.06.2020</Text>
+            </View>
           </View>
+          <Text style={styles.textBox}>Grande Promoção de Ração para seu Cachorro</Text>
           <View style={styles.fotoBox}>
             <View style={styles.item}>
-              <Text>Foto</Text>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
               <Text>R$ 38,49</Text>
             </View>
             <View style={styles.item}>
-              <Text>Foto</Text>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
               <Text>R$ 42,50</Text>
             </View>
             <View style={styles.item}>
-              <Text>Foto</Text>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
               <Text>R$ 40,99</Text>
             </View>
           </View>
@@ -62,7 +90,89 @@ const PublicacoesScreen = ({navigation}) => {
             </Text>
           </View>
         </View>
-      </View>
+        <View style={styles.publicacaoBox}>
+          <View style={styles.visivelBox}>
+            <View style={styles.visivelFromBox}>
+              <Text style={styles.visivelFrom}>Visível a Partir de: </Text>
+              <Text style={styles.statsFrom}>15.06.2020</Text>
+            </View>
+            <View style={styles.visiveUntillBox}>
+              <Text style={styles.visivelUntil}>Disponível até: </Text>
+              <Text style={styles.statsUntil}>20.06.2020</Text>
+            </View>
+          </View>
+          <Text style={styles.textBox}>Grande Promoção de Ração para seu Cachorro</Text>
+          <View style={styles.fotoBox}>
+            <View style={styles.item}>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
+              <Text>R$ 38,49</Text>
+            </View>
+            <View style={styles.item}>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
+              <Text>R$ 42,50</Text>
+            </View>
+            <View style={styles.item}>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
+              <Text>R$ 40,99</Text>
+            </View>
+          </View>
+          <View style={styles.descBox}>
+            <Text>
+              Alimento para Cães Sabor Frango Assado SPEED DOG Pacote 10,1kg
+            </Text>
+          </View>
+        </View>
+        <View style={styles.publicacaoBox}>
+          <View style={styles.visivelBox}>
+            <View style={styles.visivelFromBox}>
+              <Text style={styles.visivelFrom}>Visível a Partir de: </Text>
+              <Text style={styles.statsFrom}>15.06.2020</Text>
+            </View>
+            <View style={styles.visiveUntillBox}>
+              <Text style={styles.visivelUntil}>Disponível até: </Text>
+              <Text style={styles.statsUntil}>20.06.2020</Text>
+            </View>
+          </View>
+          <Text style={styles.textBox}>Grande Promoção de Ração para seu Cachorro</Text>
+          <View style={styles.fotoBox}>
+            <View style={styles.item}>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
+              <Text>R$ 38,49</Text>
+            </View>
+            <View style={styles.item}>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
+              <Text>R$ 42,50</Text>
+            </View>
+            <View style={styles.item}>
+              <Image
+                style={styles.itemFoto}
+                source={require('../imgs/Ração_Seca.jpg')}
+              />
+              <Text>R$ 40,99</Text>
+            </View>
+          </View>
+          <View style={styles.descBox}>
+            <Text>
+              Alimento para Cães Sabor Frango Assado SPEED DOG Pacote 10,1kg
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -76,20 +186,63 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    alignSelf: 'stretch',
+    textAlign: 'center',
+    marginTop: 5,
+    marginBottom: 5,
+    backgroundColor: '#FFFFFE',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    marginBottom: 10,
+    elevation: 2,
   },
   visivelBox: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'space-around',
     alignItems: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: '#FCFCFC',
+    height: 60,
+    borderColor: '#000000',
+    borderBottomWidth: 0.3
   },
-  visivelFrom: {},
-  visivelUntil: {},
+  visivelFromBox: {
+    flexDirection: 'row',
+    marginBottom: -25
+  },
+  visiveUntillBox: {
+    flexDirection: 'row',
+  },
   fotoBox: {
     flexDirection: 'row',
     justifyContent: 'space-around',
+    alignItems: 'flex-end',
+    alignSelf: 'stretch',
+    textAlign: 'center',
+    backgroundColor: '#FFFFFE',
+    height: 140,
+    marginBottom: 10,
+
+  },
+  text:{
+    zIndex: 5,
+  },
+  item: {
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  descBox: {},
+  itemFoto: {
+    width: 100,
+    height: 100,
+  },
+  descBox: {
+    backgroundColor: '#E6F5FF',
+    borderTopRightRadius: 25,
+    height: 60,
+  },
 })
 
 export default PublicacoesScreen;
