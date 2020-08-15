3a6aa0054627eaf8cefda60f47342074f30eecdc
Victor Ribeiro <vvictorrib@gmail.com>
2020-08-15 18:38:23 -0300

    TabNavigator icons fixed

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import TabNavigator from './src/components/navigator/TabNavigator';
 
 export default function App() {
   return (
-    <TabNavigator/>
+    <PaperProvider>
+      <TabNavigator/>
+    </PaperProvider>
   );
 }
--- src/components/navigator/TabNavigator.js
+++ src/components/navigator/TabNavigator.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+Icon.loadFont();
+
 import {footerColor, white} from '../helper/Colors';
 
 //Telas da aplicacao
@@ -19,10 +22,46 @@ export default function TabNavigator() {
         initialRouteName="Home"
         activeColor={white}
         barStyle={{backgroundColor: footerColor}}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Anúncios" component={AnuncioScreen} />
-        <Tab.Screen name="Publicações" component={PublicacoesScreen} />
-        <Tab.Screen name="Preferências" component={PreferenciasScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color}) => (
+              <Icon name="ios-home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Anúncios"
+          component={AnuncioScreen}
+          options={{
+            tabBarLabel: 'Anúncios',
+            tabBarIcon: ({color}) => (
+              <Icon name="ios-megaphone" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Publicações"
+          component={PublicacoesScreen}
+          options={{
+            tabBarLabel: 'Publicações',
+            tabBarIcon: ({color}) => (
+              <Icon name="ios-eye" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Preferências"
+          component={PreferenciasScreen}
+          options={{
+            tabBarLabel: 'Preferências',
+            tabBarIcon: ({color}) => (
+              <Icon name="ios-settings" color={color} size={26} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
